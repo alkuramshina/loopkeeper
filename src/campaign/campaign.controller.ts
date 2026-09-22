674bc9ef69a7851603d28657491fb9e436f5368f
alkuramshina <alkuramshina@gmail.com>
2026-09-22 18:14:16 +0300

    routes and guards fixes

--- src/campaign/campaign.controller.ts
+++ src/campaign/campaign.controller.ts
@@ -3,7 +3,7 @@ import { CampaignService } from './campaign.service';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { UpdateCampaignDto } from './dto/update-campaign.dto';
 
-@Controller('campaign')
+@Controller('campaigns')
 export class CampaignController {
   constructor(private readonly campaignService: CampaignService) { }
 
